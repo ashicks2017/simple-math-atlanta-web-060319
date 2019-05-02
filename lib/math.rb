53a4cb478094cafbc8fa8aa832cca54437dec560
aeisha simmons <ashicks2017@outlook.com>
2019-05-02 00:58:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,15 +9,23 @@ def subtraction(num1 =10, num2 = 5)
 end
 
 def division(num1, num2)
+  division = num1 / num2
+  return division
 end
 
 def multiplication(num1, num2)
+  multiplication = num1 * num2
+  return multiplication
 end
 
 def modulo(num1, num2)
+  modulo = num1 % num2
+  return modulo
 end
 
 def square_root(num)
+  square_root = Math.sqrt(num)
+  
 end
 
 def order_of_operation(num1, num2, num3, num4)
