88ffc16bb33d6ae645368f256b6b4f57f368c174
aeisha simmons <ashicks2017@outlook.com>
2019-05-02 00:59:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,9 @@
-def addition(num1 =5, num2 =4)
+def addition(num1, num2)
   addition = num1 + num2
   return addition
 end
 
-def subtraction(num1 =10, num2 = 5)
+def subtraction(num1, num2)
   subtraction = num1 - num2
   return subtraction
 end
